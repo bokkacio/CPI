1a2bf0db0d48821a43f32586b242c5c18f77af49
bokkacco <bokkacio@yandex.ru>
2015-03-23 17:11:26 +0300

    Initial commit

---
+++ src/ru/iate/cpi/About.java
@@ -0,0 +1,15 @@
+package ru.iate.cpi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
