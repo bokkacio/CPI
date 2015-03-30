90ba6c6678d9f7fcbd35e01999b82a133b83c0db
bokkacco <bokkacio@yandex.ru>
2015-03-30 20:08:04 +0300

    Begin to add   "DataInput" activity

---
+++ src/ru/iate/cpi/ui/activity/DataInput.java
@@ -0,0 +1,16 @@
+package ru.iate.cpi.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ru.iate.cpi.R;
+
+/**
+ * Created by sanea on 30.03.15.
+ */
+public class DataInput extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_input);
+    }
+}
