6255ec9b810b81941b6f25e533324ee1d5ef299a
bokkacco <bokkacio@yandex.ru>
2015-04-08 20:01:59 +0300

    Added "ElementsEdit" activity

---
+++ src/ru/iate/cpi/ui/activity/ElementsEdit.java
@@ -0,0 +1,16 @@
+package ru.iate.cpi.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ru.iate.cpi.R;
+
+/**
+ * Created by sanea on 08.04.15.
+ */
+public class ElementsEdit extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.elements_edit);
+    }
+}
