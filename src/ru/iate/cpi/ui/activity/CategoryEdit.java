15737b1d8e1d7f0c0084b5a29268338b0af1edf8
bokkacco <bokkacio@yandex.ru>
2015-04-14 18:59:30 +0300

    Added "CategoryEdit" activity

---
+++ src/ru/iate/cpi/ui/activity/CategoryEdit.java
@@ -0,0 +1,16 @@
+package ru.iate.cpi.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ru.iate.cpi.R;
+
+/**
+ * Created by sanea on 14.04.15.
+ */
+public class CategoryEdit extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category_edit);
+    }
+}
---
+++ src/ru/iate/cpi/ui/activity/StoreEdit.java
@@ -0,0 +1,16 @@
+package ru.iate.cpi.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ru.iate.cpi.R;
+
+/**
+ * Created by sanea on 14.04.15.
+ */
+public class StoreEdit extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store_edit);
+    }
+}
