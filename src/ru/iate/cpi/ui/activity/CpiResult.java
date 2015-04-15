80428e3bb94a83895d38f90143e5b69f33866131
bokkacco <bokkacio@yandex.ru>
2015-04-15 19:56:05 +0300

    Completed to add activities

---
+++ src/ru/iate/cpi/ui/activity/CpiResult.java
@@ -0,0 +1,16 @@
+package ru.iate.cpi.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ru.iate.cpi.R;
+
+/**
+ * Created by sanea on 15.04.15.
+ */
+public class CpiResult extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cpi_result);
+    }
+}
