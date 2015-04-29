6c3b3eb98ca91342a72c8ea7545c21e1cafe01b9
bokkacio <bokkacio@gmail.com>
2015-04-29 20:56:57 +0300

    Added another jar. Added region table class

---
+++ src/ru/iate/cpi/db/table/Region.java
@@ -0,0 +1,29 @@
+package ru.iate.cpi.db.table;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * Created by vera on 29.04.15.
+ */
+@DatabaseTable(tableName = "cpi_region")
+public class Region {
+
+    public final static String REGION_ID_FIELD = "region_id";
+    public final static String REGION_CODE_FIELD = "region_code";
+    public final static String REGION_TITLE_FIELD = "region_title";
+
+    public Region(){
+        //needed by ormlite
+    }
+
+    @DatabaseField(generatedId = true, columnName = REGION_ID_FIELD)
+    private int Id;
+
+    @DatabaseField(canBeNull = false, dataType = DataType.STRING, columnName = REGION_CODE_FIELD)
+    private String Code;
+
+    @DatabaseField(canBeNull = false, dataType = DataType.STRING, columnName = REGION_TITLE_FIELD)
+    private String Title;
+}
